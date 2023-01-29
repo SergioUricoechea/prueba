776f223009cfe57c80dbbb08c4b1f0462f3d1c94
Sergio Uricoechea <ingsergiouricoechea@gmail.com>
2023-01-29 17:05:57 -0500

    Se agrega el proyecto

---
+++ prueba.py
@@ -0,0 +1,13 @@
+from playsound import playsound
+
+def run():
+    n = int(input("ingrese las veces que quiere que se repita el audio   "))
+    i = 0
+    playsound("audio.mp3")
+    
+
+
+
+
+if __name__ == "__main__":
+    run()
